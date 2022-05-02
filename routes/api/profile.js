078ed601efb09b7d3a43c9c108c20a9d7e804f42
Abhiek187 <achaudhuri2011@yahoo.com>
2022-05-01 20:47:47 -0400

    Add route to create or update the user's profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -25,5 +26,86 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// @route   POST api/profile
+// @desc    Create or update a user profile
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company !== undefined) profileFields.company = company;
+    if (website !== undefined) profileFields.website = website;
+    if (location !== undefined) profileFields.location = location;
+    if (bio !== undefined) profileFields.bio = bio;
+    if (status.length > 0) profileFields.status = status;
+    if (githubusername !== undefined)
+      profileFields.githubusername = githubusername;
+    if (skills.length > 0) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    // Build social object
+    profileFields.social = {};
+    if (youtube !== undefined) profileFields.social.youtube = youtube;
+    if (twitter !== undefined) profileFields.social.twitter = twitter;
+    if (facebook !== undefined) profileFields.social.facebook = facebook;
+    if (linkedin !== undefined) profileFields.social.linkedin = linkedin;
+    if (instagram !== undefined) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile !== null) {
+        // Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        res.json(profile);
+      } else {
+        // Create
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(profile);
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
