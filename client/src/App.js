e6457583ce791fcbf1d03bf91d24afc6fce6330f
Abhiek187 <achaudhuri2011@yahoo.com>
2022-05-20 22:52:00 -0400

    Route to the login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <section className="container">
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 
