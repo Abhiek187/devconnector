d0b935e423b11a6d9d0af145f79fb393f29b0cf6
Abhiek187 <achaudhuri2011@yahoo.com>
2022-05-22 15:46:42 -0400

    Make the dashboard route private

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -30,9 +32,13 @@ const App = () => {
           <section className="container">
             <Alert />
             <Routes>
-              <Route exact path="/" element={<Landing />} />
-              <Route exact path="/register" element={<Register />} />
-              <Route exact path="/login" element={<Login />} />
+              <Route path="/" element={<Landing />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/dashboard"
+                element={<PrivateRoute component={Dashboard} />}
+              />
             </Routes>
           </section>
         </Fragment>
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+}) => (!isAuthenticated && !loading ? <Navigate to="/login" /> : <Component />);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
