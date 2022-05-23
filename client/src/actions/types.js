5d99b32201e59e49f416e763f32e1871c5afe9f2
Abhiek187 <achaudhuri2011@yahoo.com>
2022-05-22 20:59:03 -0400

    Create add education and experience forms

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -13,3 +13,4 @@ export const CLEAR_PROFILE = "CLEAR_PROFILE";
 // Profile
 export const GET_PROFILE = "GET_PROFILE";
 export const PROFILE_ERROR = "PROFILE_ERROR";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
