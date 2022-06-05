2c087ff1a742e37df6e41ed519841f784a25b527
Abhiek187 <achaudhuri2011@yahoo.com>
2022-06-05 18:32:08 -0400

    Retain JWT upon refresh

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,15 +1,18 @@
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
+import Spinner from "../layout/Spinner";
 
 const PrivateRoute = ({ component: Component }) => {
   const { isAuthenticated, loading } = useSelector((state) => state.auth);
 
   // Redirect to the login screen if the user needs to be authenticated
-  return !isAuthenticated && !loading ? (
-    <Navigate to="/login" />
-  ) : (
+  return loading ? (
+    <Spinner />
+  ) : isAuthenticated ? (
     <Component />
+  ) : (
+    <Navigate to="/login" />
   );
 };
 
